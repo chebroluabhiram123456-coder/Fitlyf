29d5acc02c8b706929af0b8af97c5411bbb1c544
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-03 19:22:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
+// App.js
+import React from 'react';
+import HomeScreen from './screens/HomeScreen'; // Adjust the path if you saved it elsewhere
+
+export default function App() {
+  return <HomeScreen />;
+}
 
