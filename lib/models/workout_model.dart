96665c7528828d2fb4389b2322115162169d2813
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-05 23:45:24 +0530

    Update workout_model.dart

--- lib/models/workout_model.dart
+++ lib/models/workout_model.dart
@@ -1,15 +1,13 @@
-import './exercise_model.dart';
+import 'exercise_model.dart';
 
 class Workout {
   final String id;
   final String name;
-  final String description;
   final List<Exercise> exercises;
 
   Workout({
     required this.id,
     required this.name,
-    required this.description,
     required this.exercises,
   });
 }
