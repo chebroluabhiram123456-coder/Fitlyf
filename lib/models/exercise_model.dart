9811139d3a2f7f690258e010bd292a8968b29517
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-06 21:44:43 +0530

    Update exercise_model.dart

--- lib/models/exercise_model.dart
+++ lib/models/exercise_model.dart
@@ -1,23 +1,28 @@
+import 'package:fitlyf/models/set_log_model.dart';
+
 class Exercise {
   final String id;
   final String name;
   final String targetMuscle;
-  final String? description; // <-- ADD THIS LINE
+  final String? description;
   final int sets;
   final int reps;
   bool isCompleted;
   final String? imageUrl;
   final String? videoUrl;
+  
+  List<SetLog> setsLogged;
 
   Exercise({
     required this.id,
     required this.name,
     required this.targetMuscle,
-    this.description, // <-- ADD THIS LINE
+    this.description,
     required this.sets,
     required this.reps,
     this.isCompleted = false,
     this.imageUrl,
     this.videoUrl,
+    this.setsLogged = const [],
   });
 }
