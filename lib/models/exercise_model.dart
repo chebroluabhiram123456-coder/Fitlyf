1c7255f5661c3969e41423da959ab23f3ff03a6c
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-06 20:18:29 +0530

    Update exercise_model.dart

--- lib/models/exercise_model.dart
+++ lib/models/exercise_model.dart
@@ -2,20 +2,22 @@ class Exercise {
   final String id;
   final String name;
   final String targetMuscle;
+  final String? description; // <-- ADD THIS LINE
   final int sets;
   final int reps;
   bool isCompleted;
-  final String? imageUrl; // <-- ADD THIS
-  final String? videoUrl; // <-- ADD THIS
+  final String? imageUrl;
+  final String? videoUrl;
 
   Exercise({
     required this.id,
     required this.name,
     required this.targetMuscle,
+    this.description, // <-- ADD THIS LINE
     required this.sets,
     required this.reps,
     this.isCompleted = false,
-    this.imageUrl, // <-- ADD THIS
-    this.videoUrl, // <-- ADD THIS
+    this.imageUrl,
+    this.videoUrl,
   });
 }
