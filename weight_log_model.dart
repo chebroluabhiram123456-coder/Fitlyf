a17913f92a45005232ad67771ab8e9fc463e1677
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-07 12:11:16 +0530

    Create weight_log_model.dart

---
+++ weight_log_model.dart
@@ -0,0 +1,10 @@
+// This class defines the structure for a single weight log entry.
+class WeightLog {
+  final DateTime date;
+  final double weight;
+
+  WeightLog({
+    required this.date,
+    required this.weight,
+  });
+}
