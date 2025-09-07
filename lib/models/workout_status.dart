db198193e17e04d8b4f251565702b7d3e6e8e86e
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-07 11:20:29 +0530

    Create workout_status.dart

---
+++ lib/models/workout_status.dart
@@ -0,0 +1,7 @@
+// This enum defines the possible states for a logged workout.
+// Using an enum prevents typos and makes the code more readable.
+enum WorkoutStatus {
+  Completed,
+  Skipped,
+  InProgress,
+}
