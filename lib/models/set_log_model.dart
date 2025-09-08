4fffb9331767ff6523f3072c9473b1519e805a6b
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-08 20:38:43 +0530

    Delete lib directory

--- lib/models/set_log_model.dart
+++
@@ -1,6 +0,0 @@
-class SetLog {
-  final int reps;
-  final double weight;
-
-  SetLog({required this.reps, required this.weight});
-}
--- lib/models/workout_status.dart
+++
@@ -1,7 +0,0 @@
-// This enum defines the possible states for a logged workout.
-// Using an enum prevents typos and makes the code more readable.
-enum WorkoutStatus {
-  Completed,
-  Skipped,
-  InProgress,
-}
